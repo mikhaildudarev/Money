9227eeee6225987b9f6a3707bb607e1884d334fe
Mikhail Dudarev <dev@mikhaildudarev.com>
2022-09-10 22:16:22 +0300

    Initial Commit

---
+++ Money/MoneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyApp.swift
+//  Money
+//
+//  Created by Mikhail Dudarev on 10.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct MoneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
