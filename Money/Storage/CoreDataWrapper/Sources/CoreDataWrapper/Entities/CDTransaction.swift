ff095bf112aa8a464eccb29b270197f04c0de565
Mikhail Dudarev <dev@mikhaildudarev.com>
2022-10-25 18:23:17 +0500

    first steps to add repository, based on core data

---
+++ Money/Storage/CoreDataWrapper/Sources/CoreDataWrapper/Entities/CDTransaction.swift
@@ -0,0 +1,15 @@
+//
+//  CDTransaction+CoreDataClass.swift
+//  MoneyTests
+//
+//  Created by Mikhail Dudarev on 23.10.2022.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(CDTransaction)
+public class CDTransaction: NSManagedObject {
+
+}
